22f5d76eefcb6b20ec28ffd5bb5f70ffaa03f255
Martijn Braam <martijn@brixit.nl>
2015-04-09 13:36:34 +0200

    Added server config downloading

--- configuration.py
+++ configuration.py
@@ -1,6 +1,7 @@
 import requests
 import platform
 from cpuinfo import cpuinfo
+from time import sleep
 
 
 class Configuration(object):
@@ -22,4 +23,12 @@ class Configuration(object):
         }
 
         response = requests.get(configpath, params=sysinfo)
-        # TODO:set send_throttle from response
+        server_config = response.json()
+        if not server_config["enabled"]:
+            self.logger.error("This client hasn't been accepted on the server yet.")
+            self.logger.info("Retrying in {} minutes".format(server_config["polling_time"]))
+            sleep(60 * server_config["polling_time"])
+            self.fetch()
+        else:
+            self.logger.info("Config download successful")
+            self.send_throttle = server_config["send_throttle"]
